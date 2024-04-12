1b8d3995fa76b823b83008149f8df987835f1a72
CobyGriffin <166082408+CobyGriffin@users.noreply.github.com>
2024-04-11 17:44:47 -0700

    Add project files.

---
+++ Lecture4_Prog120/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lecture4_Prog120
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            double numerator = 5;
+            double denominator = 0;
+            if(denominator !=0)
+            { Console.WriteLine(numerator / denominator); }
+            else { Console.WriteLine("your denominator cannot be 0"); }
+            //int number1
+            while (!int.TryParse(Console.ReadLine(), out int number1)
+                &&
+                !int.TryParse(Console.ReadLine(), out int number2)) ;
+        } //Main
+    } // class
+} //namespace
